4a33c8efe3fcd95175d2d1fda24f9471ad2cefe6
rookiestyle <rookiestyle@gmx.net>
2021-10-10 15:53:41 +0200

    Bump version

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle ("KeePassOTP")]
-[assembly: AssemblyDescription("Adds TOTP, HOTP & Steam OTP functionality")]
+[assembly: AssemblyDescription("Adds TOTP, HOTP, Steam & Yandex OTP functionality.")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("rookiestyle")]
 [assembly: AssemblyProduct ("KeePass Plugin")]
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.30")]
-[assembly: AssemblyFileVersion("0.30")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
